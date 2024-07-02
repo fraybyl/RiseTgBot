2a0106c1c225568141089ec556b5a9b39bd97edf
lychev <lychev@noreply.gitverse.ru>
2024-07-02 06:46:52 +0000

    На моей кровати 3 суки и все они голые

--- bot/handlers/farmers/total_weeks_handlers.py
+++ bot/handlers/farmers/total_weeks_handlers.py
@@ -5,7 +5,7 @@ from fluent.runtime import FluentLocalization
 
 from bot.core.loader import config_json, bot
 from bot.keyboards.farmers_keyboards import get_cancel_strategy_kb
-from bot.services.strategy_services.avg_drop_price import get_avg_drop
+from bot.services.strategy_services.avg_drop_price import get_avg_drop, get_rub_rate
 from bot.states.state_helper import push_state
 from bot.states.strategy_states import StrategyStates
 from bot.utils.investment_strategy import simulate_investment_strategy
@@ -24,19 +24,22 @@ async def process_total_weeks(message: Message, state: FSMContext, l10n: FluentL
         text_strategy = data.get('text_strategy')
         float_strategy = data.get('float_strategy')
         total_weeks = message.text
-        account_cost = await config_json.get_config_value('account_price')
+        account_cost = 550 / await get_rub_rate(2) # 1 - usd to rub // 2 - uah to rub
         avg_price = await get_avg_drop()
         await message.delete()
 
-        accounts_after, profit = simulate_investment_strategy(initial_accounts, account_cost, avg_price, total_weeks,
+        profit = simulate_investment_strategy(initial_accounts, account_cost, avg_price, total_weeks,
                                                               float_strategy)
-        caption_text = l10n.format_value('strategy-result',
-                                         {'name': text_strategy,
-                                          'weeks': total_weeks,
-                                          'price': avg_price,
-                                          'account_price': account_cost,
-                                          'accounts': accounts_after,
-                                          'profit': round(profit, 1)})
+        caption_text = l10n.format_value(
+            'strategy-result',
+            {
+                'name': text_strategy,
+                'weeks': int(total_weeks),
+                'price': float(avg_price), 
+                'account_price': round(float(account_cost), 2),
+                'profit': round(float(profit), 1),
+            }
+        )
 
         await bot.edit_message_caption(chat_id=message.chat.id, message_id=message_id,
                                        reply_markup=get_cancel_strategy_kb(),
