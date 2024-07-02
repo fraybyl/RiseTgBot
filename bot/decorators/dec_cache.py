a1eb96363dec3dda747f48b1fab5b2bd7556a298
lychev <pouc152@gmail.com>
2024-07-02 21:34:28 +0300

    ПЕРЕВОДЧИК ДАУН

--- bot/decorators/dec_cache.py
+++ bot/decorators/dec_cache.py
@@ -11,7 +11,7 @@ DEFAULT_TTL = 120
 async def set_redis_value(
         key: bytes | str, value: bytes | str, ttl: int | timedelta | None = DEFAULT_TTL, is_transaction: bool = False
 ) -> None:
-    """Set a value in Redis with an optional time-to-live (TTL)."""
+    """Устанавливает значение в Redis с дополнительным временем жизни (TTL)."""
     async with redis_db.pipeline(transaction=is_transaction) as pipeline:
         await pipeline.set(key, value)
         if ttl:
@@ -20,7 +20,7 @@ async def set_redis_value(
 
 
 def build_key(*args: tuple[str, any], **kwargs: dict[str, any]) -> str:
-    """Build a string key based on provided arguments and keyword arguments."""
+    """Постройте string key на основе предоставленных аргументов и аргументов ключевых слов."""
     args_str = ":".join(map(str, args))
     kwargs_str = ":".join(f"{key}={value}" for key, value in sorted(kwargs.items()))
     return f"{args_str}:{kwargs_str}"
@@ -32,7 +32,7 @@ def cached(
         key_builder: callable = build_key,
         serializer: AbstractSerializer | None = None
 ) -> callable:
-    """Caches the function's return value into a key generated with module_name, function_name, and args."""
+    """Кэширует возвращаемое значение функции в  key из module_name, function_name, и args."""
     if serializer is None:
         serializer = JSONSerializer()
     def decorator(func: callable) -> callable:
@@ -41,15 +41,12 @@ def cached(
             key = key_builder(*args, **kwargs)
             key = f"{namespace}:{func.__module__}:{func.__name__}:{key}"
 
-            # Check if the key is in the cache
             cached_value = await redis_db.get(key)
             if cached_value is not None:
                 return serializer.deserialize(cached_value)
 
-            # If not in cache, call the original function
             result = await func(*args, **kwargs)
 
-            # Store the result in Redis
             await set_redis_value(
                 key=key,
                 value=serializer.serialize(result),
@@ -68,16 +65,16 @@ async def clear_cache(
         *args: any,
         **kwargs: any,
 ) -> None:
-    """Clear the cache for a specific function and arguments.
+    """Очистить кэш для определенной функции и аргументов.
 
-    Parameters
+    Параметры
     ----------
-    - func (callable): The target function for which the cache needs to be cleared.
-    - args (any): Positional arguments passed to the function.
-    - kwargs (any): Keyword arguments passed to the function.
+    - func (вызываемая функция): Целевая функция, для которой необходимо очистить кэш.
+    - args (any): Позиционные аргументы, передаваемые функции.
+    - kwargs (any): Аргументы ключевых слов, передаваемые функции.
 
-    Keyword Arguments:
-    - namespace (str, optional): A string indicating the namespace for the cache. Defaults to "main".
+    Аргументы ключевых слов:
+    - namespace (str, optional): Строка, указывающая пространство имен для кэша. По умолчанию - "main".
     """
     namespace: str = kwargs.get("namespace", "main")
 
