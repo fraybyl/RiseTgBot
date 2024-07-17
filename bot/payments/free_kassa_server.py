be1846f083b520ad31e356a21c2aec77a672b4cf
fraybyl <fraybyl@gmail.com>
2024-07-18 01:02:28 +0300

    э у э а э у ждем активации

--- bot/payments/free_kassa_server.py
+++ bot/payments/free_kassa_server.py
@@ -1,6 +1,4 @@
 import hashlib
-import logging
-
 from aiohttp import web
 from bot.core.config import settings
 from bot.core.loader import bot
@@ -14,22 +12,16 @@ async def verify_signature(data):
 
 
 async def handle_payment_notification(request):
-    print("Received request")
     data = await request.post()
-    print(f"Request data: {data}")
     remote_addr = request.remote
-    print(f"Remote address: {remote_addr}")
 
     if remote_addr not in settings.WHITELISTED_IPS:
-        logging.info(f'remote addr: {remote_addr}, whitelist: {settings.WHITELISTED_IPS}')
-        print("Forbidden IP")
         return web.Response(status=403, text="Forbidden")
 
     if not await verify_signature(data):
-        print("Wrong signature")
         return web.Response(status=400, text="Wrong signature")
 
-    print("Processing payment")
+    # Обработка успешного платежа
     merchant_id = data['MERCHANT_ID']
     amount = data['AMOUNT']
     order_id = data['MERCHANT_ORDER_ID']
@@ -38,9 +30,7 @@ async def handle_payment_notification(request):
     user_id, quantity_product = order_id.split('-')
     await bot.send_message(int(user_id), f"Ваш платеж на сумму {amount} RUB успешно обработан!")
 
-    print("Payment processed successfully")
     return web.Response(text="YES")
-
 
 
 app = web.Application()
